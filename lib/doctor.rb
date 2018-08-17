4724c360f10c77dc63658394d4c6683478371f6b
Adam Duff <j.adam.duff@gmail.com>
2018-08-17 20:26:52 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,8 +1,22 @@
 class Doctor
   attr_accessor :name
-
+  attr_reader :appointments
+  @@all = []
+  
   def initialize(name)
     @name = name
+    self.all << self
+    @appointments = []
   end
 
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    appointment = Appointment.new(patient, self, date)
+    self.appointments << appointment
+    patient.appointments << appointment
+  end
+  
 end
