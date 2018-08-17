6a8193f563123ed837478732444b7b01458688f9
Adam Duff <j.adam.duff@gmail.com>
2018-08-17 20:05:24 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,14 @@
+class Genre
+ attr_accessor :name, :songs, :artists
+
+ def initialize(name)
+   @name = name
+   @songs = []
+ end
+
+ def artists
+   self.songs.map {|song| return song.artist if song.genre == self}
+ end
+ 
+end
+  
