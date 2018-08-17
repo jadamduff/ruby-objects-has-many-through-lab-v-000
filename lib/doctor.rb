4d4857b34b4aa4f0e7c7e5201f10ed459adfdd7f
Adam Duff <j.adam.duff@gmail.com>
2018-08-17 20:47:31 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,6 +16,7 @@ class Doctor
   def new_appointment(patient, date)
     appointment = Appointment.new(patient, self, date)
     self.appointments << appointment
+    patient.appointments << appointment
   end
 
   def patients
