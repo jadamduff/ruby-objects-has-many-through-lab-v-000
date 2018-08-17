08f3294645dd97b3dd20ea66c13b99163c288d31
Adam Duff <j.adam.duff@gmail.com>
2018-08-17 20:29:00 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,20 @@
+class Patient
+  attr_accessor :name
+  attr_reader :appointments, :doctors
+  
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def new_appointment(date, doctor)
+    appointment = Appointment.new(self, doctor, date)
+    self.appointments << appointment
+    doctor.appointments << appointment
+  end
+
+  def doctors
+    self.appointments.map {|app| return app.doctor if app.patient == self}
+  end
+  
+end
